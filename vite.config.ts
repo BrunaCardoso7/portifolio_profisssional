edf6936b9aefe58fac83686af80eaa609bb92561
BrunaCardoso7 <obitadrawing@gmail.com>
2025-02-23 11:36:21 -0300

    chore: resolve problemas do deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,43 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
-
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-  optimizeDeps: {
-    include: ['swiper/react', 'swiper/css', 'swiper/css/pagination']
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
   },
   server: {
     allowedHosts: [
       '860e-177-107-31-247.ngrok-free.app', // Apenas o domínio, sem "https://"
     ],
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ['swiper/react', 'swiper/css', 'swiper/css/pagination']
+  },
 });
+// import { defineConfig } from 'vite';
+// import tailwindcss from '@tailwindcss/vite';
+
+// export default defineConfig({
+//   plugins: [
+//     tailwindcss(),
+//   ],
+//   optimizeDeps: {
+//     include: ['swiper/react', 'swiper/css', 'swiper/css/pagination']
+//   },
+//   server: {
+//     allowedHosts: [
+//       '860e-177-107-31-247.ngrok-free.app', // Apenas o domínio, sem "https://"
+//     ],
+//   },
+// });
